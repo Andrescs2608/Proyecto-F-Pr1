8b698c4172cc81d6ee672c45b52a01c3707feac0
Andres Cedeño <136255584+Andrescs2608@users.noreply.github.com>
2024-08-14 15:25:26 -0300

    Update ProyectoPlam.py

--- ProyectoPlam.py
+++ ProyectoPlam.py
@@ -1,1 +1,11 @@
 """Proyecto PLAM """
+
+albums = [
+    ["id","Artista","Titulo","Genero"," Cantidad"],
+    [1,"Nombre del artista" , "Titulo del album","Rock",12]
+    ]
+
+cliente = [
+    ["Id","DNI","Nombre del Cliente","Telefono","Domicilio"],
+    []    
+]
